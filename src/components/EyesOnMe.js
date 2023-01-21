957793a006efe9a6b9d1d9020190cef94aca53c9
rokayaamjaad <Rukayaamged733@gmail.com>
2023-01-21 09:06:53 +0300

    finished

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
-// Code EyesOnMe Component Here
+// Code Keypad Component Here
+
+function EyesOnMe (){
+
+  const focus =()=>{
+    console.log('Good!')
+    }
+
+  const blur =()=>{
+      console.log('Hey! Eyes on me!')
+      }
+    return (
+        <div>
+   <button onBlur={blur} onFocus={focus}>Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe;
